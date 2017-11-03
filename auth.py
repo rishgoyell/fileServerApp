5c1226c284b39e27d2fc20bd1a35d6d4ae7e7ec7
Prakhar Agarwal <pkhrag@iitk.ac.in>
2017-11-03 22:00:19 +0530

    added login

--- auth.py
+++ auth.py
@@ -15,11 +15,20 @@ def signup(user, passwd, pass_repeat):
             entry = f.readline()
 
         f.write(user + " " + hashlib.md5(passwd).hexdigest() + "\n")
+        f.close()
         return "Registration Successful!!\n"
 
 
-a = raw_input("Username: ")
-b = raw_input("Pass: ")
-c = raw_input("Pass again: ")
+def login(user, passwd):
+    pwd = hashlib.md5(passwd).hexdigest()
+    f = open('cred.txt', 'a+')
+    entry = f.readline()
+    while entry != "":
+        creds = entry.split(' ')
+        if creds[0] == user and creds[1][:-1] == pwd:
+            return "Login Successful\n"
+        entry = f.readline()
 
-print signup(a, b, c)
+    f.close()
+    return "Invalid Credentials!!"
+
