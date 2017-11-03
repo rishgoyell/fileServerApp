52fb51e8b1403f7e6e5b855ea82b5ade9b505797
Piyush Bansal <puru@localhost.localdomain>
2017-11-04 00:33:48 +0530

    client and server updated

--- client.py
+++ client.py
@@ -1,16 +1,76 @@
-# Import socket module
 import socket               
  
-# Create a socket object
-s = socket.socket()         
- 
-# Define the port on which you want to connect
+class mysocket(object):
+
+	def __init__(self, sock=None):
+		if sock is None:
+			self.sock = socket.socket(
+			 socket.AF_INET, socket.SOCK_STREAM)
+		else:
+			self.sock = sock
+
+
+	def bind(self, port):
+		self.sock.bind((socket.gethostname(), port))
+
+
+	def connect(self, host, port):
+		self.sock.connect((host, port))
+
+
+	def listen(self, num_clients):
+		self.sock.listen(num_clients)
+
+
+	def accept(self):
+		c, addr = self.sock.accept()
+		clientSocket = mysocket(c)
+		return [clientSocket,addr]
+
+	def close(self):
+		self.sock.close()
+
+	def mysend(self, msg):
+		MSGLEN = len(msg)
+		length = str(MSGLEN)
+		if len(length) <= 10:
+			length = '0'*(10-len(length))+length
+		else:
+			print "increased length"
+		msg= length+msg
+		totalsent = 0
+		while totalsent < MSGLEN+10:
+			sent = self.sock.send(msg[totalsent:])
+			if sent == 0:
+				raise RuntimeError("socket connection broken")
+			totalsent = totalsent + sent
+
+	def myreceive(self):
+		MSGLEN = int(self.sock.recv(10))
+		chunks = []
+		bytes_recd = 0
+		while bytes_recd < MSGLEN:
+			chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+			if chunk == '':
+				raise RuntimeError("socket connection broken")
+			chunks.append(chunk)
+			bytes_recd = bytes_recd + len(chunk)
+		return ''.join(chunks)
+
+s = mysocket()
 port = 12345               
  
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
- 
-# receive data from the server
-print s.recv(1024)
-# close the connection
+s.connect('127.0.0.1',port)
+# print s.myreceive()
+option = raw_input(s.myreceive())
+s.mysend(option)
+print s.myreceive()
+if option == '1':
+	creds = raw_input("username:password:password_repeat\n")
+	s.mysend(creds)
+elif option == '2':
+	creds = raw_input("username:password\n")
+	s.mysend(creds)
+print s.myreceive()
+	
 s.close()       
--- server.py
+++ server.py
@@ -1,52 +1,227 @@
-# first of all import the socket library
 import socket               
 import os
 import sys
+import time
+import auth
+from user import user
 
-fp = open("users.txt","r")
-len=0
-fl = fp.readlines()
-# print fl
-usr=[]
-password=[]
-len=0
-for x in fl:
-	a,b = x.split(" ")
-	usr.append(a)
-	password.append(b)
-	len = len+1
-	# print a
+def get_option(clientsocket):
+   try:
+      clientsocket.mysend('[1] Sign Up \n[2] Sign In\n')
+   except Exception as e:
+      raise e
+   try:
+      option = clientsocket.myreceive()
+   except Exception as e:
+      raise e
+   while option not in ['1','2']:
+      try:
+         clientsocket.send('Option Not Valid: Please enter \'1\' or \'2\'')
+      except Exception as e:
+         raise e
+      try:
+         option = clientsocket.myreceive()
+      except Exception as e:
+         raise e
+   return option
 
-# next create a socket object
-s = socket.socket()         
+
+def sign_in(clientsocket):
+   curruser = user(clientsocket)
+   try:
+      clientsocket.mysend('Enter Username:Password')
+   except Exception as e:
+      raise e
+   try:
+      creds = clientsocket.myreceive()
+   except Exception as e:
+      raise e
+
+   if creds.count(':') != 1:
+      clientsocket.mysend('Information not provided in appropriate form')
+      return False
+
+   l = creds.strip('\n').split(':')
+   curruser.update_cred(l[0],l[1])
+   curruser.login()
+   return True
+
+
+      sign = auth.signup(l[0], l[1], l[2])
+      try:
+         clientsocket.mysend(sign)
+      except Exception as e:
+         raise e
+      return 1
+
+
+def sign_up():
+   return True
+
+   # elif option == '2':
+   #    try:
+   #       clientsocket.mysend('Send the credentials')
+   #    except Exception as e:
+   #       flag = 0
+   #       print "connection broke"
+   #       break;
+   #    try:
+   #       creds = clientsocket.myreceive()
+   #    except Exception as e:
+   #       raise e
+   #    l = creds.strip('\n').split(':')
+   #    sign = auth.login(l[0], l[1])
+   #    try:
+   #       clientsocket.mysend(sign)
+   #    except Exception as e:
+   #       raise e
+   #    return 2
+   # else :
+   #    clientsocket.mysend('option not recognized: Please send 1 or 2')
+   #    return 0
+
+class mysocket(object):
+
+   def __init__(self, sock=None):
+      if sock is None:
+         self.sock = socket.socket(
+          socket.AF_INET, socket.SOCK_STREAM)
+      else:
+         self.sock = sock
+
+
+   def bind(self, port):
+      self.sock.bind((socket.gethostname(), port))
+
+
+   def connect(self, host, port):
+      self.sock.connect((host, port))
+
+
+   def listen(self, num_clients):
+      self.sock.listen(num_clients)
+
+
+   def accept(self):
+      c, addr = self.sock.accept()
+      clientSocket = mysocket(c)
+      return [clientSocket,addr]
+
+   def close(self):
+      self.sock.close()
+
+   def mysend(self, msg):
+      MSGLEN = len(msg)
+      length = str(MSGLEN)
+      if len(length) <= 10:
+         length = '0'*(10-len(length))+length
+      else:
+         print "increased length"
+      msg= length+msg
+      totalsent = 0
+      while totalsent < MSGLEN+10:
+         sent = self.sock.send(msg[totalsent:])
+         if sent == 0:
+            raise RuntimeError("socket connection broken")
+         totalsent = totalsent + sent
+
+   def myreceive(self):
+      MSGLEN = int(self.sock.recv(10))
+      chunks = []
+      bytes_recd = 0
+      while bytes_recd < MSGLEN:
+         chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+         if chunk == '':
+            raise RuntimeError("socket connection broken")
+         chunks.append(chunk)
+         bytes_recd = bytes_recd + len(chunk)
+      return ''.join(chunks)
+
+
+serversocket = mysocket()       
 print "Socket successfully created"
- 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
+
 port = 12345               
- 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests 
-# coming from other computers on the network
-s.bind(('', port))        
+
+serversocket.bind(port)     
 print "socket binded to %s" %(port)
  
-# put the socket into listening mode
-s.listen(5)     
+serversocket.listen(5)     
 print "socket is listening"           
  
-# a forever loop until we interrupt it or 
-# an error occurs
 while True:
  
-   # Establish connection with client.
-   c, addr = s.accept()     
+   clientsocket, addr = serversocket.accept()     
    print 'Got connection from', addr
- 
-   # send a thank you message to the client. 
-   c.send('Thank you for connecting')
- 
-   # Close the connection with the client
-   c.close()
+   newpid = os.fork()
+   if newpid<0 :
+      print "error in forking"
+      sys.exit()
+   elif newpid==0 :
+      flag = 1
+      while True:
+         # try:
+         #    clientsocket.mysend('[1] Sign Up \n[2] Sign In\n')
+         # except Exception as e:
+         #    raise e
+         # try:
+         #    option = clientsocket.myreceive()
+         # except Exception as e:
+         #    raise e
+         # if option == '1':
+         #    try:
+         #       clientsocket.mysend('Send the credentials')
+         #    except Exception as e:
+         #       flag = 0
+         #       print "connection broke"
+         #       break;
+         #    try:
+         #       creds = clientsocket.myreceive()
+         #    except Exception as e:
+         #       raise e
+         #    l = creds.strip('\n').split(':')
+         #    sign = auth.signup(l[0], l[1], l[2])
+         #    try:
+         #       clientsocket.mysend(sign)
+         #    except Exception as e:
+         #       raise e
+         # elif option == '2':
+         #    try:
+         #       clientsocket.mysend('Send the credentials')
+         #    except Exception as e:
+         #       flag = 0
+         #       print "connection broke"
+         #       break;
+         #    try:
+         #       creds = clientsocket.myreceive()
+         #    except Exception as e:
+         #       raise e
+         #    l = creds.strip('\n').split(':')
+         #    sign = auth.login(l[0], l[1])
+         #    try:
+         #       clientsocket.mysend(sign)
+         #    except Exception as e:
+         #       raise e
+         # else :
+         #    clientsocket.mysend('option not recognized: Please send 1 or 2')
+         #    clientsocket.close()
+
+         while True:
+            option = get_option(clientsocke)
+            if option == '1':
+               if sign_up():
+                  break
+               else:
+                  continue
+            if option == '2':
+               if sign_in():
+                  break
+               else:
+                  continue
+
+
+      if not flag :
+         break
+   else:
+      clientsocket.close()
+
