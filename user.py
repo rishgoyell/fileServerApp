7f96f710dbc16af3861f107319c74dd2a59e3ea7
Prakhar Agarwal <pkhrag@iitk.ac.in>
2017-11-04 00:10:14 +0530

    added user class file

---
+++ user.py
@@ -0,0 +1,55 @@
+import auth
+import os
+from server import mysocket
+
+class user:
+
+    def __init__(self, name, passwd, sock):
+        self.name = name
+        self.pwd = passwd
+        self.path = ""
+        self.logged_in = False
+        self.sock = sock
+
+    def login(self):
+        if "Invalid" in auth.login(self.name, self.pwd):
+            del self
+            self.sock.mysend("Invalid Credentials!!\n")
+        else:
+            logged_in = True
+            path = self.name + '/'
+            self.sock.mysend("Login Successful!!\n")
+
+    def ls(self):
+        files = os.listdir(self.path)
+        flist = "\n".join(files)
+        self.sock.mysend(flist)
+
+    def readfile(self, filename):
+        if os.path.isfile(self.path + filename):
+            f = open(self.path + filename, "r")
+            self.sock.mysend(f.read())
+            f.close()
+        else:
+            self.sock.mysend("File doesn't exist!!\n")
+
+    def writefile(self, filename, data):
+        if os.path.isfile(self.path + filename):
+            self.sock.mysend("File already exists, do you want to overwrite?")
+            overwrite = self.sock.myrecieve()
+            overwrite = overwrite.strip('\n')
+            if overwrite == "yes" or overwrite == "y" or overwrite == "Y" or overwrite == "Yes":
+                f = open(self.path + filename, "w")
+                f.write(data)
+                f.close()
+                self.sock.mysend("File written successfully!!\n")
+            else:
+                self.sock.mysend("Didn't overwrite file\n")
+        else:
+            f = open(self.path + filename, "w")
+            f.write(data)
+            f.close()
+            self.sock.mysend("File written successfully!!\n")
+
+
+
